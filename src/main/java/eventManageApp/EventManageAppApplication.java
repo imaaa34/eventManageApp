c198921cf6758ada4dd65d757018d50a259a26d0
imaaa34 <oct.imd34@gmail.com>
2021-10-27 15:31:24 +0900

    initial commit

---
+++ src/main/java/eventManageApp/EventManageAppApplication.java
@@ -0,0 +1,13 @@
+package eventManageApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManageAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eventManageApp/EventManageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package eventManageApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
